76f4e083380022f243f9363542484e3956d877c7
Luka Radulovic <luka.91.rad@gmail.com>
2023-01-10 14:59:20 +0100

    [memento] memento has been added

---
+++ Behavioral/Memento/Theory/clientCode.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace paterni\Behavioral\Memento\Theory;
+
+
+/**
+ * Client code.
+ */
+$originator = new Originator("Super-duper-super-puper-super.");
+$caretaker = new Caretaker($originator);
+
+$caretaker->backup();
+$originator->doSomething();
+
+$caretaker->backup();
+$originator->doSomething();
+
+$caretaker->backup();
+$originator->doSomething();
+
+echo "\n";
+$caretaker->showHistory();
+
+echo "\nClient: Now, let's rollback!\n\n";
+$caretaker->undo();
+
+echo "\nClient: Once more!\n\n";
+$caretaker->undo();
