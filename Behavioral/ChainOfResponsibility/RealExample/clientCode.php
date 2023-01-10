63c893fccce702b90bd63ef898c426bb2952750b
Luka Radulovic <luka.91.rad@gmail.com>
2023-01-10 14:22:55 +0100

    [chain-of-responsibility] chain of responsibility has been added

---
+++ Behavioral/ChainOfResponsibility/RealExample/clientCode.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace paterni\Behavioral\ChainOfResponsibility\RealExample;
+
+/**
+ * The client code.
+ */
+$server = new Server();
+$server->register("admin@example.com", "admin_pass");
+$server->register("user@example.com", "user_pass");
+
+// All middleware are chained. The client can build various configurations of
+// chains depending on its needs.
+$middleware = new ThrottlingMiddleware(2);
+$middleware
+    ->linkWith(new UserExistsMiddleware($server))
+    ->linkWith(new RoleCheckMiddleware());
+
+// The server gets a chain from the client code.
+$server->setMiddleware($middleware);
+
+// ...
+
+do {
+    echo "\nEnter your email:\n";
+    $email = readline();
+    echo "Enter your password:\n";
+    $password = readline();
+    $success = $server->logIn($email, $password);
+} while (!$success);
---
+++ Behavioral/ChainOfResponsibility/Theory/Handler.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace paterni\Behavioral\ChainOfResponsibility\Theory;
+
+
+/**
+ * The Handler interface declares a method for building the chain of handlers.
+ * It also declares a method for executing a request.
+ */
+interface Handler
+{
+    public function setNext(Handler $handler): Handler;
+
+    public function handle(string $request): ?string;
+}
