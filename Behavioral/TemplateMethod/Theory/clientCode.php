8caeb1aa56613e519936b07093eb28b2cc0a7f25
Luka Radulovic <luka.91.rad@gmail.com>
2023-01-10 15:26:27 +0100

    [template-method] template method has been added

---
+++ Behavioral/TemplateMethod/Theory/clientCode.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace paterni\Behavioral\TemplateMethod\Theory;
+
+/**
+ * The client code calls the template method to execute the algorithm. Client
+ * code does not have to know the concrete class of an object it works with, as
+ * long as it works with objects through the interface of their base class.
+ */
+function clientCode(AbstractClass $class)
+{
+    // ...
+    $class->templateMethod();
+    // ...
+}
+
+echo "Same client code can work with different subclasses:\n";
+clientCode(new ConcreteClass1());
+echo "\n";
+
+echo "Same client code can work with different subclasses:\n";
+clientCode(new ConcreteClass2());
