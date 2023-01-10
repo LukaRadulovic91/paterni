b435e64cea2735a01452acf2e8c6682b32f0e51a
Luka Radulovic <luka.91.rad@gmail.com>
2023-01-10 14:09:48 +0100

    [singleton] singleton has been added

---
+++ Creational/Singleton/Theory/clientCode.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace paterni\Creational\Singleton\Theory;
+
+/**
+ * The client code.
+ */
+function clientCode()
+{
+    $s1 = Singleton::getInstance();
+    $s2 = Singleton::getInstance();
+    if ($s1 === $s2) {
+        echo "Singleton works, both variables contain the same instance.";
+    } else {
+        echo "Singleton failed, variables contain different instances.";
+    }
+}
+
+clientCode();
